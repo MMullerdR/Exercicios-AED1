b73adfce3140314969abf0635cd2e5a710cd17b8
MMullerdR <miguelmullerdarosa@gmail.com>
2026-04-05 21:37:19 -0300

    exercises solved

--- intersec-vet-l4.c
+++ intersec-vet-l4.c
@@ -6,10 +6,31 @@ estao em ambos os vetores. Nao deve conter numeros repetidos.*/
 #include <stdlib.h>
 
 int main () {
-    int vet[10]; 
+    int vet1[10]; 
+    int vet2[10];
+    int vets[10];
+    int cont = 0;
 
     for ( int i = 0; i < 10; i++ ) {
-        scanf("%d", vet[i]);
+        scanf("%d", &vet1[i]);
     }
-    
+
+    for ( int i = 0; i < 10; i++ ) {
+        scanf("%d", &vet2[i]);
+    }
+
+    for ( int i = 0; i < 10; i++ ) {
+        for ( int j = 0; j < 10; j++ ){
+            if( vet1[i] == vet2[j] ) {
+                vets[cont] = vet1[i];
+                cont++;
+            }
+        }
+    }
+
+    for (int i = 0; i < cont; i++) {
+        printf("%d\n", vets[i]);
+    }
+
+    return 0;
 }
