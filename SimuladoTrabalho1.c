c3d82b3e8a042157146e93b36e42cfed10d7bf25
MMullerdR <miguelmullerdarosa@gmail.com>
2026-04-19 19:41:57 -0300

    fixed: code style

--- SimuladoTrabalho1.c
+++ SimuladoTrabalho1.c
@@ -4,30 +4,25 @@
 bool lemonadeChange(int* bills, int billsSize) {
     int val_5 = 0, val_10 = 0, val_20 = 0;
 
-    for(int i = 0; i < billsSize; i++){
+    for ( int i = 0; i < billsSize; i++ ) {
        
-        if(bills[i] == 5){ 
+        if ( bills[i] == 5 ) { 
             val_5 += 5;
-        } 
-        else if(bills[i] == 10){ 
+        } else if ( bills[i] == 10 ) { 
             val_10 += 10;
             if(val_5 >= 5){
                 val_5 = val_5 - 5;
-            }
-            else{
+            } else {
                 return false;
             }
-        }
-        else if(bills[i] == 20){
+        } else if ( bills[i] == 20 ){
             val_20 += 20;
             if(val_5 >= 5 && val_10 >= 10){
                 val_5 = val_5 - 5;
                 val_10 = val_10 - 10;
-            }
-            else if( (3 * val_5) % 15 == 0 && val_5 >= 15){
+            } else if ( ( 3 * val_5 ) % 15 == 0 && val_5 >= 15 ){
                 val_5 = val_5 - 15;
-            }
-            else{
+            } else {
                 return false;
             }
         }
